ff5a3034e3bb03decfd79a547dd65f5bb92fddb5
saemyunglim <saemyung2000@naver.com>
2020-04-10 18:00:45 +0900

    jpa entitiy 구현 및 테스트

---
+++ src/test/java/japbook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package japbook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+//    @Rollback(false) <- 원래 @Test에서 @Transactional을 쓰면 테스트 후에 데이터를 롤백함. @Rollback(false)를 이용하면 롤백을 안하게 할 수 있음
+    public void testMember() throws Exception{
+        //given
+        Member member=new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+}
