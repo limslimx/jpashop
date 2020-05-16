0927845d20d6137e923763fa3f1cfd53d9e3b453
saemyunglim <saemyung2000@naver.com>
2020-05-16 10:44:57 +0900

    controller - member쪽 작성중

--- src/main/java/com/lim/jpashop/springboot/web/MemberController.java
+++ src/main/java/com/lim/jpashop/springboot/web/MemberController.java
@@ -1,10 +1,16 @@
 package com.lim.jpashop.springboot.web;
 
+import com.lim.jpashop.springboot.domain.Address;
+import com.lim.jpashop.springboot.domain.Member;
 import com.lim.jpashop.springboot.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,7 +21,23 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
-        return "";
+        return "members/createMemberfrom";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result){
+
+        if(result.hasErrors()){
+            return "members/createMemberfrom";
+        }
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 
